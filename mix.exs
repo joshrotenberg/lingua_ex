33fd271ee4051a41e2825f43d0dade489b2b2956
josh rotenberg <joshrotenberg@gmail.com>
2021-01-19 21:34:18 -0800

    package info

--- mix.exs
+++ mix.exs
@@ -7,13 +7,26 @@ defmodule Lingua.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       compilers: [:rustler] ++ Mix.compilers(),
       rustler_crates: rustler_crates(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  defp description do
+    "An Elixir wrapper around the Rust Lingua language detection library."
+  end
+
+  defp package do
+    [
+      maintainers: ["Josh Rotenberg"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/joshrotenberg/lingua_ex"}
+    ]
+  end
+
   def application do
     [
       extra_applications: [:logger]
@@ -36,8 +49,7 @@ defmodule Lingua.MixProject do
   defp deps do
     [
       {:rustler, "~> 0.21.1"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
     ]
   end
 end
