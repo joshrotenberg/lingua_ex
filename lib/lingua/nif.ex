78f58ae17cb026595788089159533f0e8f9590a1
josh rotenberg <joshrotenberg@gmail.com>
2021-01-22 17:27:43 -0800

    add init and compute

--- lib/lingua/nif.ex
+++ lib/lingua/nif.ex
@@ -3,5 +3,18 @@ defmodule Lingua.Nif do
 
   use Rustler, otp_app: :lingua, crate: "lingua_nif"
 
-  def detect_language_of(_text, _builder_option, _languages, _minimum_relative_distance), do: :erlang.nif_error(:nif_not_loaded)
+  def init(), do: error()
+
+  def detect_language_of(_text, _builder_option, _languages, _minimum_relative_distance),
+    do: error()
+
+  def compute_language_confidence_values(
+        _text,
+        _builder_option,
+        _languages,
+        _minimum_relative_distance
+      ),
+      do: error()
+
+  defp error(), do: :erlang.nif_error(:nif_not_loaded)
 end
