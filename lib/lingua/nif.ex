4377c48619cd9e10800bd88241dda20cd3ab1b10
josh rotenberg <joshrotenberg@gmail.com>
2021-02-08 21:07:09 -0800

    change order

--- lib/lingua/nif.ex
+++ lib/lingua/nif.ex
@@ -3,18 +3,6 @@ defmodule Lingua.Nif do
 
   use Rustler, otp_app: :lingua, crate: "lingua_nif"
 
-  def all_languages(), do: error()
-  def all_spoken_languages(), do: error()
-  def all_with_arabic_script(), do: error()
-  def all_with_cyrillic_script(), do: error()
-  def all_with_devanagari_script(), do: error()
-  def all_with_latin_script(), do: error()
-  def language_for_iso_code(_code), do: error()
-  def language_for_iso_code_639_1(_code), do: error()
-  def language_for_iso_code_639_3(_code), do: error()
-  def iso_code_639_1_for_language(_language), do: error()
-  def iso_code_639_3_for_language(_language), do: error()
-
   def init(), do: error()
 
   def detect_language_of(_text, _builder_option, _languages, _minimum_relative_distance),
@@ -27,6 +15,18 @@ defmodule Lingua.Nif do
         _minimum_relative_distance
       ),
       do: error()
+
+  def all_languages(), do: error()
+  def all_spoken_languages(), do: error()
+  def all_with_arabic_script(), do: error()
+  def all_with_cyrillic_script(), do: error()
+  def all_with_devanagari_script(), do: error()
+  def all_with_latin_script(), do: error()
+  def language_for_iso_code(_code), do: error()
+  def language_for_iso_code_639_1(_code), do: error()
+  def language_for_iso_code_639_3(_code), do: error()
+  def iso_code_639_1_for_language(_language), do: error()
+  def iso_code_639_3_for_language(_language), do: error()
 
   defp error(), do: :erlang.nif_error(:nif_not_loaded)
 end
