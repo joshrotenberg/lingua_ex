8d753d3d7e5c189a6baba5b1143e0112153f64e0
josh rotenberg <joshrotenberg@gmail.com>
2021-04-08 21:01:32 -0700

    bump for 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lingua.MixProject do
   def project do
     [
       app: :lingua,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
