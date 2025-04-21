6849b9cbd6822ac9275cdf515fa867139f14a03b
JonasCz <anandajonas9@gmail.com>
2025-04-21 12:40:16 +0200

    update dependency versions, remove macos x86_64

--- mix.exs
+++ mix.exs
@@ -49,8 +49,8 @@ defmodule Lingua.MixProject do
 
   defp deps do
     [
-      {:rustler_precompiled, "~> 0.6.2"},
-      {:rustler, "~> 0.29.1", optional: true},
+      {:rustler_precompiled, "~> 0.8.2"},
+      {:rustler, "~> 0.36.1", optional: true},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.16", only: :test},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
